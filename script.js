f3501ef68b26e0254cd327013a90a2131eae4c3d
Akhil <akhilagarwal1205@gmail.com>
2023-04-07 00:57:52 +0530

    clear function acquired

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const result = document.querySelector('.local-result');
 const clear = document.querySelector('.clear');
 const edit = document.querySelector('.remove');
 
+
 let answer = 0;
 let localAnswer = 0;
 
@@ -18,10 +19,19 @@ let length = 0;
 let target = "";
 let target_index = 0;
 
+let check = 0;
+
+const arr_operators = [""];
+const arr_opIndex = [0];
+const arr_localAnswer = [0];
+
 const methods = ['+', '-', '*', '/'];
 
 numbers.forEach((number) =>{
     number.addEventListener('click', ()=>{
+        if(check==1){
+            resetScreen();
+        }
         expression += number.textContent;
         input_exp.textContent = expression;
 
@@ -48,6 +58,10 @@ operators.forEach((operator)=>{
         operand2 = 0;
 
         result.textContent = "";
+
+        arr_operators.push(operat);
+        arr_opIndex.push(target_index);
+        arr_localAnswer.push(localAnswer);
     });
 });
 
@@ -62,23 +76,33 @@ clear.addEventListener('click', ()=>{
     localAnswer = 0;
     result.textContent = "";
     input_exp.textContent = "";
+    check = 0;
 })
 
 edit.addEventListener('click', ()=>{
-    if(!methods.includes(expression.charAt(length - 1))){
-        expression = expression.substring(0, length - 1);
-        input_exp.textContent = expression;
-        length = length - 1;
+    if(methods.includes(expression.charAt(length - 1))){
+        let tempLength = arr_operators.length;
+        operat = arr_operators[tempLength - 2];
+        target_index = arr_opIndex[tempLength - 2];
+        operand1 = arr_localAnswer[tempLength - 2];
 
-        createArguments();
-        localAnswer = getLocalAnswer();
+        arr_localAnswer.pop();
+        arr_opIndex.pop();
+        arr_operators.pop();
+    }
 
-        if(methods.includes(expression.charAt(length - 1))){
-            result.textContent = "";
-        }
-        else{
-            result.textContent = localAnswer;
-        }
+    expression = expression.substring(0, length - 1);
+    input_exp.textContent = expression;
+    length = length - 1;
+
+    createArguments();
+    localAnswer = getLocalAnswer();
+
+    if(methods.includes(expression.charAt(length - 1))){
+        result.textContent = "";
+    }
+    else{
+        result.textContent = localAnswer;
     }
 });
 
@@ -123,8 +147,25 @@ function getGlobalAnswer(){
     expression = "" + answer;
     result.textContent = "";
     input_exp.textContent = expression;
+    check = 1;
 }
 
  function round(num){
     return (Math.round(num*1000))/1000;
  }
+
+
+ function resetScreen(){
+    expression = "";
+    input_exp.textContent = expression;
+    result.textContent = "";
+    operat = "";
+    operand1 = 0;
+    check = 0;
+    return 0;
+ }
+
+
+ // to be solved later - edge cases
+ // first input is a operator
+ // think more
